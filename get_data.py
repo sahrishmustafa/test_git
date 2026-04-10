fa7ed34c33f6ab664544a6b08577b29441d0ba05
sahrishmustafa <sahrishmust123@gmail.com>
2026-04-10 16:46:37 +0500

    Initialize DVC

---
+++ get_data.py
@@ -0,0 +1,9 @@
+import os
+import wget
+
+# data from https://www.sciencedirect.com/science/article/pii/S2352340920303048
+
+# Download the zipped dataset
+url = 'https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/383116/rawdata_new.csv'
+file_name = "data_raw.csv"
+wget.download(url, file_name)
